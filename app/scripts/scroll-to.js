fef2dbbfdffb2163a89406145e38e31da2215f25
Thomas Yuill <thomas.yuill@gmail.com>
2014-10-24 19:14:49 -0400

    update to web 3.0

--- app/scripts/scroll-to.js
+++
@@ -1,9 +0,0 @@
-define(['jquery'], function($) {
-  $(document).on('click','[data-scroll-to]', function ( e ) {
-    var $target = $( $( e.currentTarget ).data('scroll-to') );
-    if ( $target.size() ) e.preventDefault();
-    $('html,body').animate({
-      "scrollTop": $target.offset().top
-    });
-  });
-});
--- app/scripts/utils/easingFunctions.js
+++
@@ -1,7 +0,0 @@
-define( function easingFunctions ( $ ) {
-  return {
-    "easeInOutQuad": function easeInOutQuad (t, b, c, d) {
-      return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
-    }
-  }
-});
